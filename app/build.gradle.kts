8bcc4660d0ced76a9f81ffc05c2f94e54fc152ae
Vincent-ice <1628985498@qq.com>
2026-09-14 14:20:02 +0800

    Compile editor against Android API 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.motionphotomaker"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.motionphotomaker"
